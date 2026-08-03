4ff4060db420d072a58214e3378d7a18b76ff3e8
Ronit-dev-hub <ronitserva1@gmail.com>
2026-08-03 21:48:47 +0530

    reimplemented the website logic to handle edge case of APOD api sometimes returning YouTube video URLs alongside adding error handling.

--- src/main.js
+++ src/main.js
@@ -1,25 +1,32 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
-document.querySelector("#app").innerHTML = '<p>Loading... ⏳</p>';
+document.querySelector("#app").innerHTML = '<h1>Loading... ⏳</h1>';
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-.then(response => response.json())
-.then(data => {
-  let media;
-  
-  if (data.media_type === 'image') {
-    media = `<img src="${data.url}" style="height: 400px; width: auto;" />`;
-  } else {
-    media = `<video src="${data.url}" controls></video>`;
-  }
-  
-  document.querySelector("#app").innerHTML = `
-    <h1>${data.title}</h1>
-    ${media}
-    <p>${data.explanation}</p>`;
-  }
-  
-)
+    .then(api_response => api_response.json())
+    .then(APOD => {
+        let media; // will contain the element which is going to display the APOD: <image>;<video>;<iframe>
 
+        if (APOD.media_type === 'image') {
+            media = `<image src="${APOD.url}"/>`;
+        }
 
+        else {
+            if (APOD.url.includes('youtube')) {
+                media = `<iframe src="${APOD.url}"></iframe>`;
+            }
+            else {
+                media = `<video src="${APOD.url}" controls></video>`;
+            }
+        }
+
+        // display the title, media, and explanation of APOD on the website by adding them to the index.html
+        document.querySelector("#app").innerHTML = `
+        <h1>${APOD.title}</h1>
+        ${media}
+        <p>${APOD.explanation}</p>`;
+    })
+    .catch(err => { //catch any error in executing this statement and display it on screen
+        document.querySelector("#app").innerHTML = `Error: ${err.message}`;
+    })
 
